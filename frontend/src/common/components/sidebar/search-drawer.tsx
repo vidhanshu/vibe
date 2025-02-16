ba6e0d85888e96e37f99a295e7558ee9d51139ac
vidhanshu <vidhanshu7218@gmail.com>
2025-02-16 16:01:04 +0530

    feat: add recent user searched functionality

--- frontend/src/common/components/sidebar/search-drawer.tsx
+++ frontend/src/common/components/sidebar/search-drawer.tsx
@@ -6,15 +6,20 @@ import { Skeleton } from "@/components/ui/skeleton";
 import UserAvatar from "@/src/auth/components/user-avatar";
 import { getUsers } from "@/src/users/actions/user-actions";
 import NoContent from "@/src/users/components/no-content";
+import { NSUser } from "@/src/users/types";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { User } from "lucide-react";
+import { User, X } from "lucide-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import { useDebounceValue } from "usehooks-ts";
+import { useDebounceValue, useLocalStorage } from "usehooks-ts";
 
 const SearchDrawer = ({ closeCollapse }: { closeCollapse: () => void }) => {
   const [debounced, updateDebounced] = useDebounceValue("", 1000);
+  const [recentSearches, setRecentSearches] = useLocalStorage<NSUser.User[]>(
+    "recent-searches",
+    []
+  );
 
   const qc = useQueryClient();
   const [isLoading, setIsLoading] = useState(false);
@@ -58,9 +63,57 @@ const SearchDrawer = ({ closeCollapse }: { closeCollapse: () => void }) => {
       <Separator className="mt-6" />
       <div className="px-6">
         {!debounced.trim().length ? (
-          <div className="py-6 text-muted-foreground">
-            <p>Type something to search...</p>
-          </div>
+          <>
+            {recentSearches.length ? (
+              <div>
+                <div className="flex items-center justify-between">
+                  <h1 className="mt-4 font-bold text-sm text-muted-foreground">
+                    Recent searches
+                  </h1>
+                  <button
+                    onClick={() => setRecentSearches([])}
+                    className="text-blue-500 font-bold text-sm"
+                  >
+                    Clear All
+                  </button>
+                </div>
+                <div className="space-y-4 mt-6">
+                  {recentSearches.map((user) => (
+                    <div
+                      key={user.id}
+                      className="flex items-center justify-between"
+                    >
+                      <Link
+                        onClick={closeCollapse}
+                        href={`/users/${user.username}`}
+                      >
+                        <div className="flex gap-x-4 items-center">
+                          <UserAvatar
+                            className="size-8"
+                            url={user.profilePhoto?.url}
+                            username={user.username}
+                          />
+                          {user.username}
+                        </div>
+                      </Link>
+                      <X
+                        className="size-4 cursor-pointer"
+                        onClick={() =>
+                          setRecentSearches((rs) =>
+                            rs.filter((s) => s.id !== user.id)
+                          )
+                        }
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <div className="py-6 text-muted-foreground">
+                <p>Type something to search...</p>
+              </div>
+            )}
+          </>
         ) : isLoading ? (
           <div className="space-y-4 mt-4">
             <Skeleton className="h-10" />
@@ -83,7 +136,13 @@ const SearchDrawer = ({ closeCollapse }: { closeCollapse: () => void }) => {
           data?.items?.map((user) => (
             <Link
               key={user.id}
-              onClick={closeCollapse}
+              onClick={() => {
+                closeCollapse();
+                setRecentSearches((prev) => {
+                  if (prev.some((u) => u.id === user.id)) return prev;
+                  return [...prev, user];
+                });
+              }}
               href={`/users/${user.username}`}
               className={`px-6 py-4`}
             >
